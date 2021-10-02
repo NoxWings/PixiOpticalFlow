3a20062c131aa917c796d874f4568a7dbe7c9347
David (NoxWings) García Miguel <noxwings@gmail.com>
2021-10-02 13:52:42 +0200

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,49 @@
+import commonjs from "@rollup/plugin-commonjs";
+import glslify from "rollup-plugin-glslify";
+import glslifyImport from "glslify-import";
+import livereload from "rollup-plugin-livereload";
+import resolve from "@rollup/plugin-node-resolve";
+import serve from "rollup-plugin-serve";
+import replace from "@rollup/plugin-replace";
+
+import pkg from "./package.json";
+
+const DEV = !!process.env.ROLLUP_WATCH;
+const envStr = DEV ? "development" : "production";
+
+export default {
+    input: "src/index.js",
+    output: [{
+        name: pkg.name,
+        file: pkg.main,
+        format: "iife",
+        sourcemap: true
+    }],
+    plugins: [
+        ...process.env.BUILD === 'production' ? [alias({
+            entries: [{
+                find: /^(@pixi\/([^\/]+))$/,
+                replacement: '$1/dist/esm/$2.min.js',
+            }, {
+                find: 'pixi.js',
+                replacement: 'pixi.js/dist/esm/pixi.min.js',
+            }]
+        })] : [],
+        replace({
+            "process.env.NODE_ENV": JSON.stringify(envStr)
+        }),
+        resolve({ preferBuiltins: false }),
+        commonjs(),
+        glslify({
+            transform: [glslifyImport]
+        }),
+        DEV && serve({
+            contentBase: ["dist", "static"],
+            host: "0.0.0.0",
+            port: 8080
+        }),
+        DEV && livereload({
+            watch: ["dist", "static"]
+        })
+    ]
+};
